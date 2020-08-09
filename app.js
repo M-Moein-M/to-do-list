137e0f95f60a9d72cdbb86fca987a33fc27b0132
Moein <mohammadmoein.moradi@gamil.com>
2020-08-09 10:41:01 +0430

    Added functionality of adding new task to the list and coverd entering of duplicate tasks

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
 let taskInput = document.querySelector('.task-input');
+const allTasksDiv = document.querySelector('.tasks');
+
+let allTasksArray = ['take shower', 'read 30 minutes', 'call Joe'];
+
+loadTaskList();
 
 document.querySelector('.add-task').addEventListener('click', addTask);
+document.querySelector('.task-input').addEventListener('keypress', checkEnterPress);
 
+function checkEnterPress(event) {
+    if (event.code == 'Enter')
+        addTask();
+}
 
 function addTask() {
     let task = taskInput.value;
-    taskInput.value = '';
-    if (task === '')
-        console.log("no task written to the input");
-    else
-        console.log(task);
+    taskInput.value = '';  // we make the input box empty
+    if (task === '') {
+        // console.log("no task written to the input");
+    } else if (allTasksArray.includes(task)){
+        let duplicateIndex = allTasksArray.indexOf(task);
+        let duplicaateElement = allTasksDiv.children[duplicateIndex];
+        duplicaateElement.style.setProperty('border', '3px red solid');
+        setTimeout(function () {
+            window.alert('Duplicate Task with taks number '+(duplicateIndex+1).toString());
+            duplicaateElement.style.setProperty('border', '1px #4b4bff solid');
+        }, 100);
+
+    } else {
+        allTasksArray.splice(allTasksArray.length, 0, task);
+        loadTaskList();
+    }
+
+}
+
+function allTaskDivRemoveAllChild() {
+    while (allTasksDiv.firstChild) {
+        allTasksDiv.removeChild(allTasksDiv.firstChild);
+    }
+}
+
+function loadTaskList() {
+    allTaskDivRemoveAllChild();
+    for (let i = 0; i < allTasksArray.length; i++) {
+        let pElement = document.createElement('p');
+        pElement.classList.add('task');
+        pElement.innerHTML = (i + 1).toString() + '. ' + allTasksArray[i];
+        allTasksDiv.appendChild(pElement);
+    }
 }
