83e5029cc4ef75f8b73de0345ab59c9c3a038af6
Moein <mohammadmoein.moradi@gamil.com>
2020-08-09 18:56:32 +0430

    Made project a bit more pretty and added edit modal for editing tasks using bootstrap4 and so I call it done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let taskInput = document.querySelector('.task-input');
 const allTasksDiv = document.querySelector('.tasks');
 
-let doneTasks;  // this is a sort of boolean mask array for keep track of tasks that are marked as done 0 means not done, 1 means done
+let doneTasks;  // this is a sort of boolean mask array for keeping track of tasks that are marked as done. 0 means not done, 1 means done
 let allTasksArray;
 if (JSON.parse(localStorage.getItem('allTasks')) === null)
     allTasksArray = [];
@@ -13,8 +13,8 @@ if (JSON.parse(localStorage.getItem('doneTasks')) === null)
 else
     doneTasks = JSON.parse(localStorage.getItem('doneTasks'));
 
-
 loadTaskList();
+hideAllTaskButtons();  // hide all the task buttons in the beginning and we show them when user hovers the task
 
 document.querySelector('.add-task').addEventListener('click', addTask);
 document.querySelector('.task-input').addEventListener('keypress', checkEnterPress);
@@ -22,6 +22,7 @@ window.addEventListener('beforeunload', function () {
     localStorage.setItem('allTasks', JSON.stringify(allTasksArray));
     localStorage.setItem('doneTasks', JSON.stringify(doneTasks));
 })
+document.querySelector('#edit-modal-save-changes').addEventListener('click', saveTaskChanges);
 
 function checkEnterPress(event) {
     if (event.code == 'Enter')
@@ -76,34 +77,67 @@ function ckeckDoneTask() { /// mark a task as done
 
 function moveTaskUp() {
     let taskIndex = parseInt(this.parentNode.id);  // determines task that user wants to move up
-    if (taskIndex <= 0){
+    if (taskIndex <= 0) {
         //do nothing beacause user wants to move up the 1st task
-    }else{
-        swap(taskIndex, taskIndex-1, allTasksArray);
-        swap(taskIndex, taskIndex-1, doneTasks);
+    } else {
+        swap(taskIndex, taskIndex - 1, allTasksArray);
+        swap(taskIndex, taskIndex - 1, doneTasks);
     }
     loadTaskList();
 }
 
 function moveTaskDown() {
     let taskIndex = parseInt(this.parentNode.id);  // determines task that user wants to move down
-    if (taskIndex >= allTasksArray.length - 1){
+    if (taskIndex >= allTasksArray.length - 1) {
         //do nothing beacause user wants to move down the last task
-    }else{
-        swap(taskIndex, taskIndex+1, allTasksArray);
-        swap(taskIndex, taskIndex+1, doneTasks);
+    } else {
+        swap(taskIndex, taskIndex + 1, allTasksArray);
+        swap(taskIndex, taskIndex + 1, doneTasks);
     }
     loadTaskList();
 
 }
 
-function swap(i, j, array) {
+function openEditModal() {
+    let taskIndex = parseInt(this.parentNode.id);  // determines task that user wants to change or edit
+    editedTaskIndex = taskIndex;
+    document.querySelector('#edit-modal-input').value = allTasksArray[taskIndex];
+}
+
+let editedTaskIndex;  // its value set in openEditModal and we use this to see wich task we change
+function saveTaskChanges() {
+    allTasksArray[editedTaskIndex] = document.querySelector('#edit-modal-input').value;
+    loadTaskList();
+}
+
+function swap(i, j, array) {  // swaps values in two index of input array
     let temp = array[i];
     array[i] = array[j];
     array[j] = temp;
 }
 
-function loadTaskList() {
+function hideAllTaskButtons() {
+    let allTaskButtons = document.querySelectorAll('.task-button');
+    for (let item of allTaskButtons) {
+        item.classList.add('hide');
+    }
+}
+
+function hideTaskButtons() {
+    let allTaskButtons = this.children;
+    for (let item of allTaskButtons) {
+        item.classList.add('hide');
+    }
+}
+
+function showTaskButtons() {
+    let allTaskButtons = this.children;
+    for (let item of allTaskButtons) {
+        item.classList.remove('hide');
+    }
+}
+
+function loadTaskList() {  // rebuilds all the elements in html DOM. Maybe not that efficeint but this is what it is :)
     allTaskDivRemoveAllChild();   // we clear all the children of the main division tag(.task)
     for (let i = 0; i < allTasksArray.length; i++) {
         let pElement = document.createElement('p');
@@ -122,7 +156,7 @@ function loadTaskList() {
         doneBtn.classList.add('task-button');
         doneBtn.addEventListener('click', ckeckDoneTask);
         pElement.appendChild(doneBtn);
-        
+
         let upBtn = document.createElement('button');
         upBtn.classList.add('up-button');
         upBtn.classList.add('task-button');
@@ -135,10 +169,21 @@ function loadTaskList() {
         downBtn.addEventListener('click', moveTaskDown);
         pElement.appendChild(downBtn);
 
+        let editBtn = document.createElement('button');
+        editBtn.classList.add('edit-button');
+        editBtn.classList.add('task-button');
+        editBtn.setAttribute('data-toggle', "modal");
+        editBtn.setAttribute('data-target', "#edit-modal");
+        editBtn.addEventListener("click", openEditModal);
+        pElement.appendChild(editBtn);
+
+        pElement.addEventListener('mouseenter', showTaskButtons);
+        pElement.addEventListener('mouseleave', hideTaskButtons);
         allTasksDiv.appendChild(pElement);
-        if (doneTasks[i] === 1){
+        if (doneTasks[i] === 1) {
             pElement.classList.add('done-task');
         }
 
     }
+    hideAllTaskButtons();
 }
